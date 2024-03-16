5b0108b7bfa6caa017f577b96f1e3f714fd28a01
MrBuddha <noahperry15@gmail.com>
2024-03-16 15:41:27 -0700

    React App with team State

---
+++ src/frontend/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import {TeamPage} from "./pages/TeamPage";
+import {MatchDetailCard} from "./components/MatchDetailCard";
+import {MatchSmallCard} from "./components/MatchSmallCard";
+
+function App() {
+  return (
+      <div className={"App"}>
+          <TeamPage />
+
+      </div>
+  );
+}
+
+export default App;
