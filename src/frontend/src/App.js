27b188d85bcd7b493e38da83e25c323c892e14d7
MrBuddha <noahperry15@gmail.com>
2024-03-17 19:13:23 -0700

    Completed UI elements for Team Page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,14 +1,20 @@
 //import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {TeamPage} from "./pages/TeamPage";
+import {MatchPage} from "./pages/MatchPage";
 import {MatchDetailCard} from "./components/MatchDetailCard";
 import {MatchSmallCard} from "./components/MatchSmallCard";
 
 function App() {
   return (
       <div className={"App"}>
-          <TeamPage />
-
+          <Router>
+              <Routes>
+                  <Route path={"/teams/:teamName"} element={<TeamPage />} />
+                  <Route path={"/teams/:teamName/matches/:year"} element={<MatchPage />} />
+              </Routes>
+          </Router>
       </div>
   );
 }
---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,15 @@
+import { React, useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+
+export const MatchPage = () => {
+
+
+    // if (!team || !team.teamName) {
+    //     return <h1>Team not found</h1>
+    // }
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
